3146120d1b66ccb889d93b3749e5abf11062809c
pnv13 <ivanpakhotnykh@mail.ru>
2022-05-08 16:08:12 +0300

    feat: add welcome page with auth condition

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,24 @@ import Loader from './Loader';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        {routes.map((route) => (
-          <Route
-            key={route.id}
-            path={route.path}
-            element={<Suspense fallback={<Loader />}>{route.component}</Suspense>}
-          />
-        ))}
-        <Route path="*" element={<Navigate replace to="404" />} />
-      </Route>
+      {true ? (
+        <Route path="/" element={<Layout />}>
+          {routes.map((route) => (
+            <Route
+              key={route.id}
+              path={route.path}
+              element={<Suspense fallback={<Loader />}>{route.component}</Suspense>}
+            />
+          ))}
+          <Route path="/welcome" element={<Navigate replace to="/" />} />
+        </Route>
+      ) : (
+        <Route>
+          <Route path="/welcome" element={<div>Welcome Page</div>} />
+          <Route path="*" element={<Navigate to="welcome" />} />
+        </Route>
+      )}
+      <Route path="*" element={<Navigate replace to="404" />} />
     </Routes>
   );
 }
