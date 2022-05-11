9efcdeb10bfc5d556c95770127e1471daa643bb8
pnv13 <ivanpakhotnykh@mail.ru>
2022-05-11 16:21:47 +0300

    feat: complete welcome page

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,5 +1,8 @@
 import { useAppDispatch } from '../../customHooks/redux';
 import { userSlice } from '../../store/reducers/UserSlice';
+import styles from './Welcome.module.css';
+import Task from '../../assets/welcome/task.webp';
+import TeamWork from '../../assets/welcome/team-work.webp';
 
 function Welcome() {
   const { changeTokenStatus, changeUserLoginStatus } = userSlice.actions;
@@ -12,22 +15,42 @@ function Welcome() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      Welcome Page
-      <button
-        className="border-2 border-black mb-2 hover:text-blue-400 p-1"
-        type="button"
-        onClick={changeTokenFalse}
-      >
-        The token is gone
-      </button>
-      <button
-        className="border-2 border-black hover:text-blue-400 p-1"
-        type="button"
-        onClick={changeTokenTrue}
-      >
-        The token showed up
-      </button>
+    <div className={styles.container}>
+      <div>
+        Token Interface Deleted in the future
+        <button
+          className="border-2 border-black mb-2 hover:text-blue-400 p-1"
+          type="button"
+          onClick={changeTokenFalse}
+        >
+          The token is gone
+        </button>
+        <button
+          className="border-2 border-black hover:text-blue-400 p-1"
+          type="button"
+          onClick={changeTokenTrue}
+        >
+          The token showed up
+        </button>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.images}>
+          <img src={Task} alt="task" />
+          <img src={TeamWork} alt="team-work" />
+        </div>
+        <div className={styles.text}>
+          <h3>Project Management App</h3>
+          <p>
+            A project management system is an application that helps an individual or a group of
+            developers achieve their goals. The project was part of The Rolling Scopes School React
+            course.
+          </p>
+          <p>
+            This project was created by a team of three people, you can find the authors GitHubs in
+            the footer of the application.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
