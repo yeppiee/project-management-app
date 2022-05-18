9c60bc9f2c0572026af7e57c3d92e819b7d73832
GenaVinokurov <genabrest97@gmail.com>
2022-05-18 18:03:57 +0300

    add: start do authorization

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -3,6 +3,9 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const initialState = {
   userLoginStatus: false,
   tokenStatus: false,
+  email: null,
+  token: null,
+  id: null,
 };
 
 export const userSlice = createSlice({
@@ -15,7 +18,19 @@ export const userSlice = createSlice({
     changeTokenStatus(state, action: PayloadAction<boolean>) {
       state.tokenStatus = action.payload;
     },
+    setUser(state, action) {
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+      state.id = action.payload.id;
+    },
+    removeUser(state) {
+      state.email = null;
+      state.token = null;
+      state.id = null;
+    }
   },
 });
+
+export const { setUser, removeUser } = userSlice.actions;
 
 export default userSlice.reducer;
