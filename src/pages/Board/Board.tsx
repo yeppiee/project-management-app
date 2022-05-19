500a5e7d55ebfb1ea2efafb2d1e51386b62f16b1
Denis Gusar <degusar.wot@gmail.com>
2022-05-19 18:50:16 +0300

    fix: clear board

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,25 +1,8 @@
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Modal from '../../components/Modal/Modal';
 
 function Board() {
   const { id } = useParams();
-  const [isOpen, setOpen] = useState(false);
-  return (
-    <div>
-      <button type="button" onClick={() => setOpen(true)}>
-        Open modal
-      </button>
-      {isOpen && (
-        <Modal closeModal={() => setOpen(false)}>
-          <p>{id}</p>
-          <button type="button" onClick={() => setOpen(false)}>
-            close
-          </button>
-        </Modal>
-      )}
-    </div>
-  );
+  return <div>{id}</div>;
 }
 
 export default Board;
