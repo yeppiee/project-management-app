e2c308928ec40a3a55bdd500c1600b6efc1c1ef9
Denis Gusar <degusar.wot@gmail.com>
2022-05-19 15:58:18 +0300

    fix: add UseEffect to remove eventListener after closing modal window

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { ModalPropsType } from '../../Types/modalTypes';
 import styles from './Modal.module.css';
 
 function Modal({ isOpen, children, closeModal }: ModalPropsType) {
   const portalDiv = document.getElementById('portal') as HTMLElement;
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') closeModal();
+    });
+    return document.removeEventListener('keydown', (e) => {
+      if (e.key === 'Escape') closeModal();
+    });
+  }, [closeModal]);
+
   if (!isOpen) return null;
   return ReactDOM.createPortal(
     <div role="button" tabIndex={0} className={styles.overlay} onClick={closeModal}>
--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Modal from '../../components/Modal/Modal';
 
 function Board() {
   const { id } = useParams();
-  return <div>Open modal {id}</div>;
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <div>
+      <button type="button" onClick={() => setOpen(true)}>
+        Open modal
+      </button>
+      <Modal isOpen={isOpen} closeModal={() => setOpen(false)}>
+        <p>{id}</p>
+        <button type="button" onClick={() => setOpen(false)}>
+          close
+        </button>
+      </Modal>
+    </div>
+  );
 }
 
 export default Board;
