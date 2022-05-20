3918120ccb77d775f02f4f0ea4bdfbd6dfb1778e
Denis Gusar <degusar.wot@gmail.com>
2022-05-20 12:19:20 +0300

    feat rebaae develop

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,8 +1,29 @@
-import { useParams } from 'react-router-dom';
+/* import { useParams } from 'react-router-dom'; */
+import { FormattedMessage } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
+import styles from './Board.module.css';
 
 function Board() {
-  const { id } = useParams();
-  return <div>{id}</div>;
+  const navigate = useNavigate();
+  const onClickNavigate = () => navigate(-1);
+  /*  const { id } = useParams(); */
+  return (
+    <div className={styles.board}>
+      <div className={styles.header}>
+        <button type="button" className={styles.headerButton} onClick={onClickNavigate}>
+          <FormattedMessage id="board-back" />
+        </button>
+      </div>
+      <div className={styles.boardsContainer}>
+        <button type="button" className={styles.buttonCreateColumn}>
+          <span className={styles.plus} />
+          <p>
+            <FormattedMessage id="board-add-column" />
+          </p>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Board;
