f6f472c2beee4f177a7a17bc9a24653e5e0aa7b1
pnv13 <ivanpakhotnykh@mail.ru>
2022-06-14 17:19:14 +0300

    fix: fix layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import BurgerMenu from '../components/BurgerMenu';
 import CreateBoard from '../components/BoardForms/CreateBoard';
 import Modal from '../components/Modal';
@@ -7,12 +7,19 @@ import { useAppDispatch, useAppSelector } from '../customHooks/redux';
 import Footer from './Footer';
 import Header from './Header';
 import { boardFormSlice } from '../store/reducers/BoardFormSlice';
+import { userSlice } from '../store/reducers/UserSlice';
 
 function Layout() {
   const { createBoardModalIsOpen } = useAppSelector((state) => state.boardFormSlice);
   const { changeCreateBoardModalIsOpen } = boardFormSlice.actions;
+  const { changeUserLoginStatus } = userSlice.actions;
   const dispatch = useAppDispatch();
   const [isActive, setIsActive] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === '/welcome') dispatch(changeUserLoginStatus(false));
+  }, [changeUserLoginStatus, dispatch, pathname]);
 
   const handleChange = () => setIsActive(!isActive);
   const setInactive = () => setIsActive(false);
